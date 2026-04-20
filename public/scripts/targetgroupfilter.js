0f6680f1c3ce09abe2f8808c5d5f2a8298810c7b
IsaacEswa <isaaceswa@gmail.com>
2026-04-20 18:10:07 +0200

    client-side Fetch uitproberen

--- public/scripts/targetgroupfilter.js
+++ public/scripts/targetgroupfilter.js
@@ -1,3 +1,12 @@
-document.getElementById('filter-button').hidden = true;
-document.getElementById('targetgroup-select').addEventListener('change', function () { this.form.submit() });
-document.getElementById('sort-select').addEventListener('change', function () { this.form.submit() })
+// document.getElementById('filter-button').hidden = true;
+// document.getElementById('targetgroup-select').addEventListener('change', function () { this.form.submit() });
+// document.getElementById('sort-select').addEventListener('change', function () { this.form.submit() })
+
+
+async function getTeams() {
+    const teamResponse = await fetch('https://fdnd.directus.app/items/person/?fields=team&filter[team][_neq]=null&sort=team&groupBy=team')
+    const teamResponseJSON = await teamResponse.json()
+    console.log(teamResponseJSON)
+}
+
+getTeams()
